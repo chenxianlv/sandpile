cae4bbb6d71c62e76c39105538dfbefa1e895b78
decoy <a122186291@gmail.com>
2023-03-06 23:09:46 +0800

    前端提交(2023/3/6) 1. 添加：笔记项目页添加搜索框，用于筛选笔记项目

--- webDevelop/trunk/src/views/Note/NoteProjectSelect.vue
+++ webDevelop/trunk/src/views/Note/NoteProjectSelect.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
 import { listProjectsAPI } from '@/api/note';
-import { onActivated, ref } from 'vue';
+import { computed, onActivated, ref } from 'vue';
 import dayjs from 'dayjs';
 import type { NoteProject } from '@/views/Note/hooks';
 import type { NormalResponse } from '@/common/axios';
 import { useRouter } from 'vue-router';
 import $bus from '@/common/eventBus';
+import { Search } from '@element-plus/icons-vue';
 
 const noteProjects = ref<NoteProject[]>([]);
 const router = useRouter();
-
-const handleRowClick = ({ id }: { id: number }) => {
-    router.push({ name: 'detail', params: { id } });
-};
 
 onActivated(() => {
     $bus.emit('headerCollapse', false);
@@ -21,12 +18,29 @@ onActivated(() => {
 listProjectsAPI().then((res: NormalResponse) => {
     noteProjects.value = res?.data?.data?.noteProjects;
 });
+
+const handleRowClick = ({ id }: { id: number }) => {
+    router.push({ name: 'detail', params: { id } });
+};
+
+const filterString = ref<string>('');
+const noteProjectsAfterFilter = computed(() => {
+    return noteProjects.value.filter((project) => {
+        if (project.projectName.includes(filterString.value)) return true;
+        if (project.createUserName.includes(filterString.value)) return true;
+    });
+});
 </script>
 
 <template>
     <div class="container">
-        <!-- todo 搜索框-->
-        <el-table :data="noteProjects" @row-click="handleRowClick">
+        <el-input
+            class="search-input"
+            placeholder="搜索项目"
+            :prefix-icon="Search"
+            v-model="filterString"
+        />
+        <el-table :data="noteProjectsAfterFilter" @row-click="handleRowClick">
             <el-table-column prop="projectName" label="笔记项目名">
             </el-table-column>
             <el-table-column
@@ -51,6 +65,7 @@ listProjectsAPI().then((res: NormalResponse) => {
 
 <style lang="less" scoped>
 .container {
+    box-sizing: border-box;
     width: 100%;
     max-width: 1000px;
     height: 100%;
@@ -60,11 +75,23 @@ listProjectsAPI().then((res: NormalResponse) => {
     background-color: @bg-color-top-layer;
     box-shadow: @normal-box-shadow;
 }
+.search-input {
+    --el-input-border-radius: 8px;
+    --el-input-hover-border-color: @base-border-color;
+
+    float: right;
+    width: 430px;
+    height: 32px;
+    margin: 10px 0;
+}
 .el-table {
     --el-table-bg-color: @bg-color-top-layer;
     --el-table-header-bg-color: @bg-color-top-layer;
     --el-table-tr-bg-color: @bg-color-top-layer;
     --el-table-row-hover-bg-color: @bg-color-table-row-hover;
+
+    --el-table-header-text-color: @regular-font-color;
+    --el-table-text-color: @regular-font-color;
 
     :deep(.el-scrollbar) {
         cursor: pointer;
