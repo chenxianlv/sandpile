28460fb9b695be8b1e9d5de5d5c38eaad9556852
decoy <a122186291@gmail.com>
2023-04-02 18:15:01 +0800

    后端提交(2023/4/2) 1. 修改：因部署需要，更新部分文件

--- src/main/resources/sand.sql
+++ src/main/resources/sand.sql
@@ -5,7 +5,7 @@ CREATE TABLE `menu_item_info`
     `menu_index`     INT UNIQUE         NOT NULL COMMENT '菜单项序号（序号小的排在前）',
     `menu_name`      VARCHAR(50)        NULL COMMENT '菜单项的名称（用于展示在页面上）',
     `menu_key`       VARCHAR(50)        NOT NULL COMMENT '菜单项标识',
-    `delete_flag`   BOOLEAN   DEFAULT FALSE COMMENT '是否逻辑删除',
+    `delete_flag`    BOOLEAN   DEFAULT FALSE COMMENT '是否逻辑删除',
     `create_time`    TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `create_user_id` BIGINT             NULL COMMENT '创建用户的ID',
     `update_time`    TIMESTAMP          NULL COMMENT '修改时间',
@@ -27,7 +27,7 @@ CREATE TABLE `note_project_info`
 (
     `id`             BIGINT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT COMMENT '笔记项目id',
     `project_name`   VARCHAR(255)       NOT NULL COMMENT '笔记项目名',
-    `delete_flag`   BOOLEAN   DEFAULT FALSE COMMENT '是否逻辑删除',
+    `delete_flag`    BOOLEAN   DEFAULT FALSE COMMENT '是否逻辑删除',
     `create_time`    TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `create_user_id` BIGINT             NULL COMMENT '创建用户的ID',
     `update_time`    TIMESTAMP          NULL COMMENT '修改时间',
@@ -44,7 +44,7 @@ CREATE TABLE `note_info`
     `directory_path` VARCHAR(500)       NOT NULL COMMENT '笔记所在目录的路径，以/分隔，如"/note1"表示笔记位于项目根目录下的note1目录中',
     `file_path`      VARCHAR(500)       NOT NULL COMMENT '笔记文件的物理位置，如"/docs/math.md"',
     `project_id`     BIGINT             NOT NULL COMMENT '所属笔记项目的id',
-    `delete_flag`   BOOLEAN   DEFAULT FALSE COMMENT '是否逻辑删除',
+    `delete_flag`    BOOLEAN   DEFAULT FALSE COMMENT '是否逻辑删除',
     `create_time`    TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `create_user_id` BIGINT             NULL COMMENT '创建用户的ID',
     `update_time`    TIMESTAMP          NULL COMMENT '修改时间',
@@ -60,7 +60,7 @@ CREATE TABLE `system_params`
     `param_key`      VARCHAR(255)       NOT NULL COMMENT '参数名',
     `param_value`    VARCHAR(500)       NULL COMMENT '参数值',
     `description`    VARCHAR(500)       NULL COMMENT '参数描述',
-    `delete_flag`   BOOLEAN   DEFAULT FALSE COMMENT '是否逻辑删除',
+    `delete_flag`    BOOLEAN   DEFAULT FALSE COMMENT '是否逻辑删除',
     `create_time`    TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `create_user_id` BIGINT             NULL COMMENT '创建用户的ID',
     `update_time`    TIMESTAMP          NULL COMMENT '修改时间',
@@ -129,55 +129,3 @@ CREATE TABLE `user_role_access`
     `access_id` BIGINT UNSIGNED NOT NULL COMMENT '权限ID',
     PRIMARY KEY (`id`)
 ) COMMENT '用户权限表';
-
-
-INSERT INTO `menu_item_info`(`menu_index`, `menu_name`, `menu_key`)
-VALUES (1, '学习笔记', 'noteDisplay');
-
-
-
-# INSERT INTO `system_params`(`param_key`, `param_value`, `description`)
-# VALUES ('file_base_path', 'http://localhost:9135/files',
-#         '文件的基本存放路径，项目中文件的路径信息为相对路径，会与该参数拼接后形成完整路径');
-
-
-# test
-INSERT INTO `note_project_info`(`id`, `project_name`, `create_user_id`)
-VALUES (1, '前端笔记', 1);
-
-INSERT INTO `note_info`(`name`, `directory_path`, `file_path`, `project_id`, `create_user_id`)
-VALUES ('CSS', '/', '/CSS.md', 1, 1),
-       ('HTML', '/', '/HTML.md', 1, 1),
-       ('JavaScript', '/', '/JavaScript.md', 1, 1),
-       ('Node.js', '/', '/Node.js.md', 1, 1),
-       ('TypeScript', '/', '/TypeScript.md', 1, 1),
-       ('React', '/库', '/库/React.md', 1, 1),
-       ('Three.js', '/库', '/库/Three.js.md', 1, 1),
-       ('Vue', '/库', '/库/Vue.md', 1, 1),
-       ('Vue3', '/库', '/库/Vue3.md', 1, 1),
-       ('常用小型库', '/库', '/库/常用小型库.md', 1, 1),
-       ('配置文件', '/库', '/库/配置文件.md', 1, 1);
-
--- ----------
--- 密码为1234
--- ----------
-INSERT INTO `user_user`(`id`, `user_account`, `user_name`, `user_password`, `create_user_id`)
-VALUES (1, 'admin', 'administrator', '$2a$10$G6zACWgQzdmCGgdlFYc0WeMPI8L0zwrfOAs7gwl0qWCgdD4Kw.YzG', 1),
-       (2, 'cxl', 'chenxianlv', '$2a$10$G6zACWgQzdmCGgdlFYc0WeMPI8L0zwrfOAs7gwl0qWCgdD4Kw.YzG', 1);
-
-INSERT INTO `user_role`(`id`, `role_name`, `create_user_id`)
-VALUES (1, 'root', 1),
-       (2, 'manager', 1);
-
-INSERT INTO `user_access`(`id`, `access_name`, `access_url`)
-VALUES (1, 'updateProject', '/api/note/updateProject'),
-       (2, 'addProject', '/api/note/addProject'),
-       (3, 'deleteProject', '/api/note/deleteProject');
-
-INSERT INTO `user_user_role`(`user_id`, `role_id`)
-VALUES (1, 1),
-       (2, 2);
-
-INSERT INTO `user_role_access`(`role_id`, `access_id`)
-VALUES (1, 1),(1, 2),(1, 3),
-       (2, 1),(2, 2),(2, 3);
