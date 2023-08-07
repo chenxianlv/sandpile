fd66982d0894884c62f52add100232aba5426501
陈显律 <88878493+chenxianlv@users.noreply.github.com>
2023-08-07 12:39:17 +0800

    fix: 修复数据库时间错误问题

--- server/src/main/java/org/sand/model/vo/note/NoteProjectVO.java
+++ server/src/main/java/org/sand/model/vo/note/NoteProjectVO.java
@@ -1,10 +1,9 @@
 package org.sand.model.vo.note;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.sand.model.vo.common.BasicVO;
 
-import java.util.Date;
+import java.sql.Timestamp;
 
 @Data
 public class NoteProjectVO extends BasicVO {
@@ -15,7 +14,6 @@ public class NoteProjectVO extends BasicVO {
 
     private String createUserName;
 
-    @JsonFormat(timezone = "${project.time-zone}", pattern="yyyy-MM-dd HH:mm:ss")
-    private Date createTime;
+    private Timestamp createTime;
 
 }
