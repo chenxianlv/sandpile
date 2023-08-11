0acba2774003c5dbb0a5ea9aca96e5ce3fc2a732
陈显律 <88878493+chenxianlv@users.noreply.github.com>
2023-08-11 18:45:01 +0800

    refactor: 整合API配置至配置文件

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,6 +1,25 @@
-export default {
+const config: {
     /**
      * 是否在开发模式下打印请求信息
      */
-    printInDev: true,
+    PRINT_IN_DEV: boolean;
+
+    /**
+     * http请求url的前缀
+     */
+    BASE_URL: '/api';
+
+    /**
+     * 请求该数组内的url，响应有错误时，不会交由axios拦截器进行统一错误处理
+     * url值即为调用axios方法时传入的url属性，不包含baseUrl
+     */
+    CUSTOM_ERROR_HANDLE_URL: string[];
+} = {
+    PRINT_IN_DEV: true,
+
+    BASE_URL: '/api',
+
+    CUSTOM_ERROR_HANDLE_URL: [],
 };
+
+export default config;
