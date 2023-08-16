8b5efe5f3fb349002666eb28db64b9bd622de403
decoy <a122186291@gmail.com>
2023-08-16 17:14:23 +0800

    refactor: 依赖注入采用构造函数形式

--- server/src/main/java/org/sand/service/user/impl/RoleServiceImpl.java
+++ server/src/main/java/org/sand/service/user/impl/RoleServiceImpl.java
@@ -1,19 +1,19 @@
 package org.sand.service.user.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.AllArgsConstructor;
 import org.sand.mapper.user.RoleMapper;
 import org.sand.model.po.user.RolePO;
 import org.sand.service.user.RoleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class RoleServiceImpl extends ServiceImpl<RoleMapper, RolePO> implements RoleService {
 
-    @Autowired
-    private RoleMapper roleMapper;
+    private final RoleMapper roleMapper;
 
     public List<RolePO> listRolesByUserId(Long userId){
         return roleMapper.listRolesByUserId(userId);
