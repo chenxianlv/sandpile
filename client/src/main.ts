007c289dac76aa8587f0597094950a87a7fabcd1
陈显律 <88878493+chenxianlv@users.noreply.github.com>
2023-08-31 15:35:43 +0800

    fix: 权限更改完，页面上的笔记项目未根据新权限判断是否显示

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,7 +6,8 @@ import '@/styles/cleanUp.less';
 import '@/styles/common.less';
 import baseConfig from '@/config/base';
 import { i18n } from '@/lang';
-import 'virtual:svg-icons-register';
+import vAuth from '@/directives/vAuth';
+import 'virtual:svg-icons-register'; // vite svg加载器所需指令
 
 import App from './App.vue';
 import router from './router';
@@ -21,5 +22,7 @@ app.use(createPinia());
 app.use(router);
 app.use(ElementPlus);
 app.use(i18n);
+
+app.directive('Auth', vAuth);
 
 app.mount('#app');
