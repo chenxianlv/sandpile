83148a3db517cc63a32b91a26eafd1eb0e96cf71
陈显律 <88878493+chenxianlv@users.noreply.github.com>
2023-09-07 16:53:45 +0800

    fix: 修复删除文件夹时，内部文件和文件夹未被删除的bug

--- server/src/main/java/org/sand/service/note/NoteFolderService.java
+++ server/src/main/java/org/sand/service/note/NoteFolderService.java
@@ -4,4 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import org.sand.model.po.note.NoteFolderPO;
 
 public interface NoteFolderService extends IService<NoteFolderPO> {
+
+    boolean deleteFolder(Long id);
+
 }
--- server/src/main/java/org/sand/service/note/impl/NoteFolderServiceImpl.java
+++ server/src/main/java/org/sand/service/note/impl/NoteFolderServiceImpl.java
@@ -1,11 +1,41 @@
 package org.sand.service.note.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.AllArgsConstructor;
 import org.sand.mapper.note.NoteFolderMapper;
 import org.sand.model.po.note.NoteFolderPO;
+import org.sand.model.po.note.NotePO;
 import org.sand.service.note.NoteFolderService;
+import org.sand.service.note.NoteService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class NoteFolderServiceImpl extends ServiceImpl<NoteFolderMapper, NoteFolderPO> implements NoteFolderService {
+
+    NoteService noteService;
+
+    @Override
+    public boolean deleteFolder(Long id) {
+        if (!removeById(id)) {
+            return false;
+        }
+
+        // 删除文件夹下的笔记
+        LambdaQueryWrapper<NotePO> lqw = new LambdaQueryWrapper<>();
+        lqw.eq(NotePO::getFolderId, id);
+        List<NotePO> notePOs = noteService.list(lqw);
+        notePOs.forEach(notePO -> noteService.removeById(notePO.getId()));
+
+        // 删除文件夹下的文件夹
+        LambdaQueryWrapper<NoteFolderPO> lqw2 = new LambdaQueryWrapper<>();
+        lqw2.eq(NoteFolderPO::getFolderId, id);
+        List<NoteFolderPO> noteFolderPOs = list(lqw2);
+        noteFolderPOs.forEach(noteFolderPO -> deleteFolder(noteFolderPO.getId()));
+
+        return true;
+    }
 }
